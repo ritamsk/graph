9d5aa9742cecaab9a09262372071884e3780f0a0
ritamsk <32394222+ritamsk@users.noreply.github.com>
2018-04-12 02:03:40 +0300

    Update dijkstra.py

--- task2/dijkstra.py
+++ task2/dijkstra.py
@@ -1,24 +1,24 @@
 import math
 
-def adjDijkstra(n, start, w, weight):
+def adjDijkstra(n, start, adjs, weight):
     INF=math.inf
-    dist = [INF] * n
-    dist[start] = 0
-    prev = [None] * n
-    used = [False] * n
-    min_dist = 0
-    min_vertex = start
-    while min_dist < INF:
-        i = min_vertex
-        used[i] = True
-        for j in w.get(i):
-            if dist[i] + weight.get(i.__str__() + ', ' + j.__str__()) < dist[j]:
-                dist[j] = dist[i] + weight.get(i.__str__() + ', ' + j.__str__())
-                prev[j] = i
+    d = [INF] * n
+    d[start] = 0
+    p = [None] * n
+    seen = [False] * n
+    min_d = 0
+    min_node = start
+    while min_d < INF:
+        i = min_node
+        seen[i] = True
+        for j in adjs.get(i):
+            if d[i] + weight.get(i.__str__() + ', ' + j.__str__()) < d[j]:
+                d[j] = d[i] + weight.get(i.__str__() + ', ' + j.__str__())
+                p[j] = i
         min_dist = INF
         for i in range(n):
-            if not used[i] and dist[i] < min_dist:
-                min_dist = dist[i]
-                min_vertex = i
+            if not seen[i] and d[i] < min_d:
+                min_d = d[i]
+                min_node = i
 
-    return dist, prev
+    return d, p
